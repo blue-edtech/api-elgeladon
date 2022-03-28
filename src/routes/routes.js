7a36249a6fabc2e632cafe9366685813e48089f2
codethi <docencia.thiago@gmail.com>
2022-03-28 14:20:10 -0300

    carrinho

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 const route = require('express').Router();
 const controllerPaletas = require('../controllers/paleta.controller');
+const controllerCarinho = require('../controllers/carrinho.controller');
 
 const {
   validId,
-  validObjectBody
+  validObjectBody,
+  validObjectBodyCarrinho,
 } = require('../middlewares/paleta.middleware');
 
 route.get('/all-paletas', controllerPaletas.findAllPaletasController);
@@ -27,6 +29,17 @@ route.delete(
   '/delete-paleta/:id',
   validId,
   controllerPaletas.deletePaletaController,
+);
+
+route.get('/all-carrinho', controllerCarinho.findAllCarrinhoController);
+route.post(
+  '/create-carrinho',
+  validObjectBodyCarrinho,
+  controllerCarinho.createManyItemsCarrinhoController,
+);
+route.delete(
+  '/finish-carrinho',
+  controllerCarinho.deleteAllItemsCarrinhoController,
 );
 
 module.exports = route;
